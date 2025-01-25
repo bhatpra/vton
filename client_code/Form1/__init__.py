620ccaac819698e33b8fd5b5e17edb3320094b65
pbhat66@gmail.com <pbhat66@gmail.com>
2025-01-25 19:25:21 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,7 +11,7 @@ class Form1(Form1Template):
 
         # Title
         self.label_title = Label(
-            text="Stable Diffusion Try-On Demo (2-step polling)",
+            text="DSI Try-On Demo",
             align="center",
             font_size=20,
             bold=True
@@ -58,10 +58,53 @@ class Form1(Form1Template):
         self.cloth_media = None
         self.fetch_url = None
 
-    def file_loader_user_change(self, file, **event_args):
-        if file:
-            self.user_media = file
-            self.image_user_preview.source = file
+def file_loader_user_change(self, file, **event_args):
+    """
+    Compress ONLY the user image client-side.
+    """
+    if not file:
+        return
+
+    # 1) Get the DOM node of the entire FileLoader
+    file_loader_node = anvil.js.get_dom_node(self.file_loader_user)
+
+    # 2) Query inside it for the real <input type="file">
+    js_file_input = file_loader_node.querySelector("input[type='file']")
+    if js_file_input and js_file_input.files and js_file_input.files.length > 0:
+        js_file_obj = js_file_input.files[0]  # The raw JavaScript File object
+
+        # 3) Call our JS compression function
+        promise = anvil.js.call_js("compressImage", js_file_obj, 600)  # maxWidth=600
+
+        def on_success(data_url):
+            # data_url is "data:image/jpeg;base64,..."
+            comma_idx = data_url.find(",")
+            if comma_idx < 0:
+                alert("Invalid data URL after compression.")
+                return
+            meta_part = data_url[:comma_idx]  # e.g. "data:image/jpeg;base64"
+            b64_data = data_url[comma_idx+1:]
+
+            # Determine content_type from meta_part
+            if "image/png" in meta_part:
+                content_type = "image/png"
+            else:
+                content_type = "image/jpeg"
+
+            import base64
+            raw_bytes = base64.b64decode(b64_data)
+            compressed_media = BlobMedia(content_type, raw_bytes, name="compressed_user.jpg")
+
+            self.user_media = compressed_media
+            self.image_user_preview.source = compressed_media
+            print(f"Compressed user image size: {len(raw_bytes)} bytes")
+
+        def on_error(err):
+            alert(f"Error compressing user image: {err}")
+
+        anvil.js.await_promise(promise, on_success, on_error)
+    else:
+        alert("Could not find the actual file input. No file selected?")
 
     def file_loader_cloth_change(self, file, **event_args):
         if file:
