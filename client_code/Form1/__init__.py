1af1133d179ebef03d7d65b9a7e8eb0f8bde8f86
pbhat66@gmail.com <pbhat66@gmail.com>
2025-01-27 12:27:03 +0000

    Edited settings, ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,13 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.microsoft.auth
+import anvil.facebook.auth
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 import anvil.server
 import anvil.js
 import base64
